87ac32e9f4c158389044fbe76af8250a1bf73eaa
yrus <shahrushi199891@gmail.com>
2021-01-25 00:53:48 +0530

    changed logic for picking element

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { vec3, mat4 } from 'https://cdn.skypack.dev/gl-matrix';
+import { vec3, vec4, mat4 } from 'https://cdn.skypack.dev/gl-matrix';
 import Shader from './shader.js';
 import vertexShaderSrc from './vertex.js';
 import fragmentShaderSrc from './fragment.js';
@@ -11,7 +11,7 @@ let controlBtn = document.getElementById('modeBtn');
 let controlModeNames = ['Drawing','Instance-Transformation','Scene-Transformation'];
 let shapeBtn = document.getElementById('shapeBtn');
 let shapeNames = ['Rectangle','Square','Circle'];
-// let captImageBtn = document.getElementById('captureImage');
+let clearBtn = document.getElementById('clearBtn');
 
 
 const renderer = new Renderer();
@@ -50,6 +50,7 @@ let tYCon = gui.add(transformSettings, 'translateY', -1.0, 1.0, 0.020001);
 let scaleCon = gui.add(transformSettings, 'scale', 0.0, 2.0, 0.02);
 let rotCon = gui.add(transformSettings, 'rotationAngle', -180, 180);
 gui.hide();
+
 // Convert mouse click to coordinate system as understood by webGL
 renderer.getCanvas().addEventListener('click', (event) =>
 {
@@ -58,7 +59,6 @@ renderer.getCanvas().addEventListener('click', (event) =>
 	let mouseX = event.clientX;
 	let mouseY = event.clientY;
 
-	 // Needed in some cases ?
 	let rect = renderer.getCanvas().getBoundingClientRect();
 	mouseX = mouseX - rect.left;
 	mouseY = mouseY - rect.top;
@@ -81,16 +81,46 @@ renderer.getCanvas().addEventListener('click', (event) =>
 			primitives[currActiveElemIndex].resetColor();
 		}
 		currActiveElemIndex = -1;
-		let minDist = 100000;
-		let tempIndex = 0;
+		let tempIndex = -1;
+
+		// Selects Active element on basis of shortest distance from element center 
+		// let minDist = 100000;
+		// primitives.forEach(function(primitive, index, arr){
+		// 	console.log(primitive.constructor.name);
+		// 	let dist = Math.pow(primitive.getPos(0) - clipCoordinates[0],2) + 
+		// 				Math.pow(primitive.getPos(1) - clipCoordinates[1],2);
+		// 	if(dist < minDist){
+		// 		minDist = dist;
+		// 		tempIndex = index;
+		// 	}
+		// });
+
+
+		// Selects Active element according to the element outline
+		let tempMatrix = mat4.create();
+		let mouseCoordVec4 = vec4.fromValues(clipCoordinates[0], clipCoordinates[1],0 ,1);
 		primitives.forEach(function(primitive, index, arr){
-			let dist = Math.pow(primitive.getPos(0) - clipCoordinates[0],2) + 
-						Math.pow(primitive.getPos(1) - clipCoordinates[1],2);
-			if(dist < minDist){
-				minDist = dist;
-				tempIndex = index;
+			if(primitive.constructor.name === 'Rectangle'){
+				mat4.identity(tempMatrix);
+				mat4.scale(tempMatrix, tempMatrix, vec3.fromValues(1/primitive.transform.getScale(), 1/primitive.transform.getScale(), 1/primitive.transform.getScale()));
+				mat4.rotate(tempMatrix, tempMatrix, -primitive.transform.getRotate(), vec3.fromValues(0,0,1));
+				mat4.translate(tempMatrix, tempMatrix, vec3.fromValues(-primitive.getPos(0), -primitive.getPos(1), 0));
+
+				mat4.multiply(tempMatrix, tempMatrix, mouseCoordVec4);
+
+				// console.log(tempMatrix);
+				if(tempMatrix[0] > -primitive.width/2 && tempMatrix[0] < primitive.width/2 &&
+					tempMatrix[1] > -primitive.height/2 && tempMatrix[1] < primitive.height/2){
+					tempIndex = index;
+				}
+			}else if(primitive.constructor.name === 'Circle'){
+				if( Math.pow(primitive.getPos(0) - clipCoordinates[0],2) + 
+						Math.pow(primitive.getPos(1) - clipCoordinates[1],2) < Math.pow(primitive.radius, 2))
+					tempIndex = index;
 			}
 		});
+		if(tempIndex == -1)	return;
+
 		primitives[tempIndex].setColor([0,1,0]);
 
 		// console.log(primitives[tempIndex]);
@@ -164,6 +194,7 @@ window.addEventListener('keydown', function (event){
 
 modeBtn.addEventListener("click", changeControlMode); 
 shapeBtn.addEventListener("click", changeShapeMode); 
+clearBtn.addEventListener("click", clearCanvas); 
 // captImageBtn.addEventListener("click", function(){
 // 	captImageBtn.setAttribute('href', gl.canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
 // });
@@ -224,6 +255,10 @@ function changeControlMode(){
 function changeShapeMode(){
 	shapeMode = (shapeMode + 1) % 3;
 	shapeBtn.innerHTML = 'Shape : ' + shapeNames[shapeMode];
+}
+
+function clearCanvas(){
+	primitives.splice(0, primitives.length);
 }
 
 //Draw loop
